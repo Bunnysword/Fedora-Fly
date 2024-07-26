1af3879c83353a404e0435b0b854d50126726fda
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-07-27 00:50:37 +0300

    Update nobarapi.sh

--- nobarapi.sh
+++ nobarapi.sh
@@ -6,8 +6,8 @@ sudo dnf copr enable -y principis/NoiseTorch && sudo dnf in -y noisetorch
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf in -y code #VSC
 sudo dnf in -y papirus-icon-theme
-sudo dnf rm -y firefox elisa kmousetool kcharselect kamoso neochat skanpage okular mediawriter 
-sudo dnf config-manager --set-enabled google-chrome && sudo dnf in -y transmission-qt google-chrome-stable #Chrome
+sudo dnf rm -y elisa kmousetool kcharselect kamoso neochat skanpage okular mediawriter 
+sudo dnf in -y transmission-qt
 flatpak install flathub -y portproton
 sudo cp services/wakeup-disable_GPP0.service /etc/systemd/system/ && sudo systemctl enable wakeup-disable_GPP0.service && sudo systemctl start wakeup-disable_GPP0.service #B550 fix
 mkdir /home/bunnysword/.config/systemd/ && mkdir /home/bunnysword/.config/systemd/user && mv services/noisetorch.service /home/bunnysword/.config/systemd/user/ && systemctl --user daemon-reload && systemctl --user start noisetorch && systemctl --user enable noisetorch #noisetorch
