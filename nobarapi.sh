634acc256ba0b11886cff4814f4df917214f07f8
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-07-27 00:38:26 +0300

    Update nobarapi.sh

--- nobarapi.sh
+++ nobarapi.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+#Nobara post install
 sudo dnf config-manager --add-repo https://terra.fyralabs.com/terra.repo && sudo dnf in -y discord
 sudo dnf copr enable -y principis/NoiseTorch && sudo dnf in -y noisetorch
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf in -y code #VSC
 sudo dnf in -y papirus-icon-theme
+sudo dnf rm -y firefox elisa kmousetool kcharselect kamoso neochat skanpage okular mediawriter 
 sudo dnf config-manager --set-enabled google-chrome && sudo dnf in -y transmission-qt google-chrome-stable #Chrome
 flatpak install flathub -y portproton
 sudo cp services/wakeup-disable_GPP0.service /etc/systemd/system/ && sudo systemctl enable wakeup-disable_GPP0.service && sudo systemctl start wakeup-disable_GPP0.service #B550 fix
