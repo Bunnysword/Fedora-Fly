46cc4415229c3fff8bea531809dcd4d1aa019439
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-07-27 00:55:45 +0300

    Update nobarapi.sh

--- nobarapi.sh
+++ nobarapi.sh
@@ -7,7 +7,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "
 sudo dnf in -y code #VSC
 sudo dnf in -y papirus-icon-theme
 sudo dnf rm -y kmousetool kcharselect kamoso neochat skanpage okular mediawriter 
-sudo dnf in -y transmission-qt
+sudo dnf in -y transmission-qt piper nvtop vlc goverlay 
 flatpak install flathub -y portproton
 sudo cp services/wakeup-disable_GPP0.service /etc/systemd/system/ && sudo systemctl enable wakeup-disable_GPP0.service && sudo systemctl start wakeup-disable_GPP0.service #B550 fix
 mkdir /home/bunnysword/.config/systemd/ && mkdir /home/bunnysword/.config/systemd/user && mv services/noisetorch.service /home/bunnysword/.config/systemd/user/ && systemctl --user daemon-reload && systemctl --user start noisetorch && systemctl --user enable noisetorch #noisetorch
