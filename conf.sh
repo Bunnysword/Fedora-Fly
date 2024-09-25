f9595157a4a08aef5b22939fb06c36b32b3c1d1f
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-09-25 19:16:12 +0300

    Add files via upload

---
+++ conf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir ~/.config/OpenRGB && mv conf/1.orp ~/.config/OpenRGB/ #OpenRGB profile
+mkdir ~/.config/fastfetch && mv conf/config.jsonc ~/.config/fastfetch/ && fastfetch #fastfetch conf
+mkdir ~/.config/MangoHud && mv conf/MangoHud.conf ~/.config/MangoHud/ #MangoHud
+sudo mv conf/environment /etc
+mkdir ~/.steam/ && mkdir ~/.steam/steam/ && mv conf/steam_dev.cfg ~/.steam/steam/ #steam tips
+sudo cp services/wakeup-disable_GPP0.service /etc/systemd/system/ && sudo systemctl enable wakeup-disable_GPP0.service && sudo systemctl start wakeup-disable_GPP0.service #B550 fix
+git clone https://github.com/JustTemmie/steam-presence && mv conf/config.json steam-presence/ && cd steam-presence/ && ./installer.sh && cd ..
---
+++ eoss.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+yay -Sy fastfetch code pamac goverlay mangohud discord spoofdpi openrgb nvtop
+chmod 750 conf.sh && ./conf.sh
