73498fc3f70010e1a760611191248a7a8d21bc90
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-10-01 20:10:13 +0300

    Update nvdriver.sh

--- nvdriver/nvdriver.sh
+++ nvdriver/nvdriver.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 #nvutils
-#flatpak install flathub com.leinardi.gwe for only X11
-#nvdriver 560.35.03
-sudo dnf --enablerepo=rpmfusion-nonfree-updates-testing install akmod-nvidia-open xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686
-#sudo dnf --enablerepo=rpmfusion-nonfree-updates-testing install akmod-nvidia xorg-x11-drv-nvidia-libs.i686
-
-flatpak install -y org.freedesktop.Platform.GL.nvidia-560-35-03
+#sudo dnf --enablerepo=rpmfusion-nonfree-updates-testing install akmod-nvidia-open xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686
+sudo dnf in akmod-nvidia xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686
+#flatpak install -y org.freedesktop.Platform.GL.nvidia-560-35-03
 ####nvidia-kms
 sudo cp nvidia-kms.conf /etc/modprobe.d/
