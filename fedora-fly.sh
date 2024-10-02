68831026f72e9662148c84de0a4192f048d300b1
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-10-02 21:17:47 +0300

    Update fedora-fly.sh

--- fedora-fly.sh
+++ fedora-fly.sh
@@ -19,10 +19,6 @@ sudo dnf config-manager --add-repo https://terra.fyralabs.com/terra.repo && sudo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf in -y code #VSC
 sudo dnf in -y papirus-icon-theme #Papirus icons
-if [ -n "$XDG_CURRENT_DESKTOP" ]; then
-  case "$XDG_CURRENT_DESKTOP" in
-    GNOME)
-echo 'GNOME Tips'
 sudo sed -i "6i AutomaticLoginEnable=True\nAutomaticLogin=bunnysword" /etc/gdm/custom.conf #autologin in GDM
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' && gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
@@ -30,28 +26,17 @@ sudo dnf copr enable -y trixieua/mutter-patched
 sudo firewall-cmd --zone=public --permanent --add-port=1714-1764/tcp && sudo firewall-cmd --zone=public --permanent --add-port=1714-1764/udp && sudo systemctl restart firewalld.service #GSCONNECT
 sudo dnf rm -y mediawriter rhythmbox evince yelp gnome-characters gnome-logs gnome-tour gnome-photos gnome-maps gnome-weather gnome-font-viewer gnome-contacts gnome-clocks gnome-calendar gnome-boxes libreoffice* power-profiles-daemon
 sudo dnf in -y transmission-gtk gnome-tweaks
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub -y com.mattjakeman.ExtensionManager
 #EXTENSIONS
 gnome-extensions disable background-logo@fedorahosted.org
-      ;;
-    KDE)
-echo 'KDE Tips'
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita' && plasma-apply-colorscheme BreezeDark
-sudo dnf rm -y kolourpaint mediawriter okular skanpage kmahjongg kmines kpat akregator kmail ktnef sieveeditor neochat contactprintthemeeditor contactthemeeditor pimdataexporter dragonplayer elisa kamoso korganizer kcharselect kmousetool
-sudo dnf in -y transmission-qt
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-      ;;
-  esac
-fi
 #Install_Apps
 sudo dnf in -y timeshift goverlay steam kdenlive vlc htop redhat-lsb-core inxi fastfetch protontricks openssl discord noisetorch openrgb nvtop --allowerasing
 sudo dnf update -y --refresh #Update
 #Flatpak
-flatpak install flathub -y com.obsproject.Studio portproton net.davidotek.pupgui2
-#com.heroicgameslauncher.hgl
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub -y com.obsproject.Studio portproton net.davidotek.pupgui2 com.mattjakeman.ExtensionManager
 #Services
 sudo systemctl mask plymouth-quit-wait.service && systemctl disable livesys-late.service && systemctl disable livesys.service && systemctl disable rpcbind.service && systemctl disable lvm2-monitor.service && systemctl disable NetworkManager-wait-online.service #Disable
-chmod 750 conf.sh && ./conf.sh
-#NVIDIAMOMENTO
+#configurations
+chmod 750 conf.sh && ./configurations.sh
+#NVIDIA
 sudo dnf in akmod-nvidia xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686
