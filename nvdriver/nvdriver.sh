2f15ef6dfca45611b534cfe43bc9a1f3506c3ec9
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-10-02 21:10:58 +0300

    Delete nvdriver directory

--- nvdriver/nvdriver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#nvutils
-#sudo dnf --enablerepo=rpmfusion-nonfree-updates-testing install akmod-nvidia-open xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686
-sudo dnf in akmod-nvidia xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686
-#flatpak install -y org.freedesktop.Platform.GL.nvidia-560-35-03
-####nvidia-kms
-sudo cp nvidia-kms.conf /etc/modprobe.d/
