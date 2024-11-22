83ece85e9a5bd991276cd680eb3559b95dbe0baa
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-11-22 19:08:04 +0300

    Update fedora-fly.sh

--- fedora-fly.sh
+++ fedora-fly.sh
@@ -16,7 +16,6 @@ sudo sed -i "13i exclude=kernel*" /etc/yum.repos.d/fedora-updates.repo && sudo d
 sudo dnf copr enable -y principis/NoiseTorch && sudo dnf in -y noisetorch #Noisetorch
 sudo dnf config-manager --add-repo https://terra.fyralabs.com/terra.repo && sudo dnf in -y discord #Discord Terra
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-sudo dnf in -y code #VSC
 sudo dnf in -y papirus-icon-theme #Papirus icons
 sudo sed -i "6i AutomaticLoginEnable=True\nAutomaticLogin=bunnysword" /etc/gdm/custom.conf #autologin in GDM
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
@@ -24,11 +23,10 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' && gsettings 
 sudo dnf copr enable -y trixieua/mutter-patched
 sudo firewall-cmd --zone=public --permanent --add-port=1714-1764/tcp && sudo firewall-cmd --zone=public --permanent --add-port=1714-1764/udp && sudo systemctl restart firewalld.service #GSCONNECT
 sudo dnf rm -y mediawriter rhythmbox evince yelp gnome-characters gnome-logs gnome-tour gnome-photos gnome-maps gnome-weather gnome-font-viewer gnome-contacts gnome-clocks gnome-calendar gnome-boxes libreoffice* power-profiles-daemon
-sudo dnf in -y transmission-gtk gnome-tweaks
 #EXTENSIONS
 gnome-extensions disable background-logo@fedorahosted.org
 #Install_Apps
-sudo dnf in -y timeshift goverlay steam kdenlive vlc htop redhat-lsb-core inxi fastfetch protontricks openssl discord noisetorch openrgb nvtop --allowerasing
+sudo dnf in -y timeshift goverlay steam kdenlive vlc htop redhat-lsb-core inxi fastfetch openssl discord noisetorch openrgb nvtop transmission-gtk gnome-tweaks code --allowerasing
 sudo dnf update -y --refresh #Update
 #Flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
