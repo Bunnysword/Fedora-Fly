32546b3f48da343a446ce05aed37befbec40794d
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-03-23 13:20:39 +0300

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -13,22 +13,24 @@ sudo dnf in -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh2
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf copr enable -y codifryed/CoolerControl
 sudo dnf copr enable -y bieszczaders/kernel-cachyos
+sudo dnf copr enable -y lizardbyte/stable
+
 #appinstall
-sudo dnf in -y mangohud steam kdenlive mpv htop redhat-lsb-core inxi fastfetch openrgb nvtop transmission-qt code coolercontrol ffmpeg --allowerasing
+sudo dnf in -y mangohud steam kdenlive mpv htop redhat-lsb-core inxi fastfetch openrgb nvtop transmission-qt code coolercontrol ffmpeg fish sunshine --allowerasing
 #update
 sudo dnf install -y kernel-cachyos kernel-cachyos-devel-matched
+sudo grubby --set-default /boot/vmlinuz*cachyos*
 sudo dnf update -y --refresh
 #flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub -y com.obsproject.Studio net.davidotek.pupgui2 org.telegram.desktop dev.vencord.Vesktop
+flatpak install flathub -y com.obsproject.Studio net.davidotek.pupgui2 org.telegram.desktop
 #disableservices
 sudo systemctl mask plymouth-quit-wait.service
 systemctl disable rpcbind.service
 systemctl disable NetworkManager-wait-online.service
 #nvidiadriver
-sudo dnf in rpmfusion-nonfree-release-rawhide
 sudo dnf in -y akmod-nvidia xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686 nvidia-vaapi-driver
-
+sudo grubby --args="rd.driver.blacklist=nouveau" --update-kernel=ALL
 #conf for kde
 mkdir ~/.config/fastfetch && wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/kde/config.jsonc && mv config.jsonc ~/.config/fastfetch/
 wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/kde/bugs.png && mv bugs.png ~/.config/fastfetch/
