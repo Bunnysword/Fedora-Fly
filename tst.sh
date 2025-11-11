545a833092b3e7572ab5ba0c1efbbf0da79c4846
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-11-11 19:23:55 +0300

    Create tst.sh

---
+++ tst.sh
@@ -0,0 +1,1 @@
+sudo dnf in -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
