33f47ef9500ca1df2fb647e418fe8418f50a9ade
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-11-11 19:33:48 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,4 +1,6 @@
+#rpmfushion
 sudo dnf in -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
-sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+#codecs
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y & sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+#nvidia
 sudo dnf in -y akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs.i686 libva-nvidia-driver.{i686,x86_64}
